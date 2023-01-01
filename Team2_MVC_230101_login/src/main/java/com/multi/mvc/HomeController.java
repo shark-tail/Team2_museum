e8105b87dfcedab2a76c00f84117553dded0f4c0
shark-tail <114653074+shark-tail@users.noreply.github.com>
2023-01-02 04:10:00 +0900

    Add files via upload
    230102 메인에 공지사항게시판 완.

--- Team2_MVC_230101_login/src/main/java/com/multi/mvc/HomeController.java
+++ Team2_MVC_230101_login/src/main/java/com/multi/mvc/HomeController.java
@@ -15,8 +15,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.multi.mvc.board.model.service.BoardService;
+import com.multi.mvc.board.model.vo.Board;
 import com.multi.mvc.common.util.PageInfo;
 import com.multi.mvc.heritage.model.service.HeritageService;
 import com.multi.mvc.heritage.model.vo.Heritage;
@@ -37,13 +39,36 @@ public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model, HttpSession session) {
+	public String home(Locale locale, Model model, HttpSession session, @RequestParam Map<String, String> param) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 //		Member loginMember = memberService.login("admin", "1212");
 //		session.setAttribute("loginMember", loginMember);
 		// test();
 		List<Heritage> list = heService.selectCollectionList();
 		model.addAttribute("list", list);
+		
+// 바뀐부분 시작  230102
+		//공지게시판
+		System.out.println("리스트 요청, param : " + param);
+		int page = 1;
+		// 탐색할 맵을 선언
+		Map<String, String> searchMap = new HashMap<String, String>();
+		try {
+			String searchValue = param.get("searchValue");
+			if(searchValue != null && searchValue.length() > 0) {
+				String searchType = param.get("searchType");
+				searchMap.put(searchType, searchValue);
+			}
+			page = Integer.parseInt(param.get("page"));
+		} catch (Exception e) {}
+		searchMap.put("type", param.get("type"));
+		
+		int boardCount = service.getBoardCount(searchMap);
+		PageInfo pageInfo = new PageInfo(page, 7, boardCount, 7);
+		
+		List<Board> list2 = service.getBoardList(pageInfo, searchMap);
+		model.addAttribute("list2", list2);
+// 바뀐부분 끝	 230102
 		return "index";
 	}
 	
