52bdf2177f42ab847e0344d7889fc233295d049a
bibibi01 <116268910+bibibi01@users.noreply.github.com>
2023-01-02 05:16:42 +0900

    Update HReply.java

--- Team2_MVC_230101_login/src/main/java/com/multi/mvc/heritage/model/vo/HReply.java
+++ Team2_MVC_230101_login/src/main/java/com/multi/mvc/heritage/model/vo/HReply.java
@@ -13,8 +13,10 @@ public class HReply {
 	private int hrno;
 	private int hno; //문화재
 	private int uno; //유저
+	private String nickName; // 닉네임
 	private String hrContent; //리플내용
 	private Date hrCreateDate;
 	private Date hrModifyDate;
 	private String hrStatus;
+
 }
