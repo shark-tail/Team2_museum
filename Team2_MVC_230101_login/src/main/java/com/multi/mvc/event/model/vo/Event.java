49f32b1125e8d2f37225c163083cd3e5a861feb3
aexaekaso <46701146+aexaekaso@users.noreply.github.com>
2023-01-02 01:54:40 +0900

    Create Event.java

---
+++ Team2_MVC_230101_login/src/main/java/com/multi/mvc/event/model/vo/Event.java
@@ -0,0 +1,22 @@
+package com.multi.mvc.event.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Event {
+	private int e_no;
+	private String subTitle;
+	private String subContent;
+	private String sdate;
+	private String edate;
+	private String subDesc;
+	private String subDesc1;
+	private String image;
+	private String sido;
+	private String gogun;
+	private String subDate;
+}                  
