1745fbbaa0c742d2106a097675f660aeba5c548f
aexaekaso <46701146+aexaekaso@users.noreply.github.com>
2023-01-02 20:45:08 +0900

    ex-collection-*.jsp 페이지 맵핑

--- Team2_MVC_230101_login/src/main/java/com/multi/mvc/collection/controller/MyCollectionController.java
+++ Team2_MVC_230101_login/src/main/java/com/multi/mvc/collection/controller/MyCollectionController.java
@@ -72,5 +72,36 @@ public class MyCollectionController {
 	public String CollectionList3() {
 		return "collection/collection-list_item";
 	}
+	
+	
+	//검색 전 백범힝구의 컬렉션 상세 조회
+	@RequestMapping("/ex-collection-detail-1")
+	public String exDetail1() {
+		return "collection/ex-collection-detail-1";
+	}
+	//검색 후 전이만갑오개혁의 컬렉션 상세 조회
+	@RequestMapping("/ex-collection-detail-2")
+	public String exDetail2() {
+		return "collection/ex-collection-detail-2";
+	}
+	//전이만갑오개혁의 컬렉션 전체모드로 보기
+	@RequestMapping("ex-collection-view-all-1")
+	public String exViewAll1() {
+		return "collection/ex-collection-view-all-1";
+	}
+	//컬렉션 검색 후 컬렉션 리스트
+	@RequestMapping("/ex-collection-list")
+	public String exList1(@RequestParam("searchValue") String searchValue) {
+		log.info("검색 요청 " + searchValue);
+		if (searchValue.equals("경주")) {
+			log.info("닉네임 검색");
+			return "collection/ex-collection-list-1";
+		}
+		if (searchValue.equals("청자")) {
+			log.info("문화재 검색");
+			return "collection/ex-collection-list-2";
+		}
+		return "collection/ex-collection-main-1";
+	}
 
 }
